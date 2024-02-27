9de2b9856d551c0b6b8ce51e7d342a5b363f0c59
Evan <egarbacz@cdtrojan.com>
2024-02-27 15:35:27 -0500

    Scans all the files in a specified directory.

---
+++ scan_directory.py
@@ -0,0 +1,10 @@
+import os
+def scanFiles(directory):
+	for root, _, files in os.walk
+		for file in files:
+		 file_path = os.path.join(root, file)
+		 print(file_path)
+
+if __name__ == "__main__":
+	specified_folder = r""
+	scanFiles(specified_foler)
