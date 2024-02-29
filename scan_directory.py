0349c11d6b19c7cfb097d3091a1b554998f63787
Evan <egarbacz@cdtrojan.com>
2024-02-29 12:26:06 -0500

    Adds the ability for the scan_directory function to check if a file is corrupt or not.

--- scan_directory.py
+++ scan_directory.py
@@ -1,10 +1,17 @@
 import os
+from file_corrupt import isFileCorrupt
+
 def scanFiles(directory):
 	for root, _, files in os.walk
 		for file in files:
-		 file_path = os.path.join(root, file)
-		 print(file_path)
+			file_path = os.path.join(root, file)
+			print(file_path)
+			if isFileCorrupt(file_path, signiture = b"\x89PNG\r\n\x1a\n"):
+				print(file_path + "Is not corrupted")
+			else:
+				print(file_path + "File is corrupted")
+		
 
 if __name__ == "__main__":
 	specified_folder = r""
-	scanFiles(specified_foler)
+	scanFiles(specified_folder)
